94d11b32bb16e2f57e70eba2ca05025d93aec043
넵플러스 <kmc901206@gmail.com>
2022-08-09 11:57:42 +0900

    7. 리싸이클러뷰 실 데이터 반영

--- app/src/main/java/com/neppplus/jickbangcopy_recyclerview_20220809/adapters/RoomRecyclerViewAdapter.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_recyclerview_20220809/adapters/RoomRecyclerViewAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.neppplus.jickbangcopy_recyclerview_20220809.R
 import com.neppplus.jickbangcopy_recyclerview_20220809.datas.RoomData
@@ -16,6 +17,15 @@ class RoomRecyclerViewAdapter(
     inner class MyViewHolder(view : View) : RecyclerView.ViewHolder(view) {
         fun bind(item : RoomData) {
 //            itemView - item(한칸에 들어갈 RoomData) 간의 연결(bind)
+            val priceTxt = itemView.findViewById<TextView>(R.id.priceTxt)
+            val addressTxt = itemView.findViewById<TextView>(R.id.addressTxt)
+            val levelTxt = itemView.findViewById<TextView>(R.id.levelTxt)
+            val descriptionTxt = itemView.findViewById<TextView>(R.id.descriptionTxt)
+
+            priceTxt.text = item.getFormattedPrice()
+            addressTxt.text = "${item.address}, "
+            levelTxt.text = item.getFormattedLevel()
+            descriptionTxt.text = item.description
         }
     }
 
